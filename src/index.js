98d3b6271bc64ef3abf1fd2ffde2503dcd1c4cf9
giogio165 <103351870+giogio165@users.noreply.github.com>
2023-08-09 21:10:50 +0900

    에러 수정을 위한 커밋

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import "./index.css";
 import App from "./App";
-import { ReactDOM } from "react-dom";
-
+import ReactDOM from "react-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
