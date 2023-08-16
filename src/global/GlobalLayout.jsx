58b34b41a14ec07802cf73bf2e18a5e5609a8a88
prgmr99 <johnyeom24@naver.com>
2023-08-16 23:21:07 +0900

    merge 했음

--- src/global/GlobalLayout.jsx
+++ src/global/GlobalLayout.jsx
@@ -8,18 +8,17 @@ const GlobalLayout = ({ children }) => {
 export default GlobalLayout;
 
 const StGlobalContainer = styled.div`
-  max-width: calc(100vw - 400px);
   margin: 0 auto;
   width: 100%;
   background-color: #000;
   color: white;
   height: 100%;
-  padding: 80px 300px 0px 330px;
+  padding: 80px 300px 0px 550px;
   display: flex;
   flex-direction: column;
 
   @media (max-width: 1700px) {
-    max-width: 160rem;
+    max-width: calc(1680px - 50%);
   }
   @media (max-width: 1560px) {
     max-width: 1500px;
