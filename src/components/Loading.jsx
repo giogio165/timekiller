f7dd70a778603a2c2960b928edd127bc20a8b3d1
prgmr99 <johnyeom24@naver.com>
2023-09-26 21:48:47 +0900

    [feat] #33 Loading 화면 추가

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -15,19 +15,26 @@ export default Loading;
 
 const SLoading = styled.div`
   position: absolute;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   top: 0;
   left: 0;
   background: black;
-  z-index: 999;
+  z-index: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  box-sizing: border-box;
+  overflow-x: hidden;
+
+  img {
+    margin-left: 10%;
+  }
 `;
 
 const SLoadingText = styled.div`
+  margin-left: 10%;
   font: 1rem "Noto Sans KR";
   text-align: center;
 `;
